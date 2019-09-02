da1d37ba3ec6774218daf5640d0096281b65c83c
Patrick Heintz <patrickmheintz@gmail.com>
2019-09-02 13:07:23 -0700

    Add Solution

---
+++ EnemiesSpawnFaster/EnemiesSpawnFaster.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+
+namespace EnemiesSpawnFaster
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.lloyd.EnemiesSpawnFaster", "Enemies Spawn Faster", "1.0.0")]
+    public class EnemiesSpawnFaster : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            On.RoR2.CombatDirector.Simulate += (orig, self, deltaTime) =>
+            {
+                self.maxSeriesSpawnInterval = new float();
+                self.maxSeriesSpawnInterval = 0.5f;
+                self.maxSeriesSpawnInterval = new float();
+                self.maxSeriesSpawnInterval = 2f;
+                orig(self, deltaTime);
+            };
+        }
+    }
+}
